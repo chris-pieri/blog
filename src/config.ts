be293aaf5434e12a1a4ae94c107e7f5c1c94f4ca
Chris Pieri <chrispieri3@gmail.com>
2025-08-01 09:43:00 -0400

    Update colors

--- src/config.ts
+++ src/config.ts
@@ -14,10 +14,10 @@ export const SITE = {
   editPost: {
     enabled: false,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/chris-pieri/blog/edit/main/",
   },
   dynamicOgImage: false,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "America/New_York", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
